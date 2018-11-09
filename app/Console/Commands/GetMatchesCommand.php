66cdb34260657029112416baf3c55d1d86d3d957
alaeddine thamine <alaeddine.thamine.prestataire@sfr.com>
2018-11-09 15:25:01 +0100

    Command get match for better perf Mapping remove diff

--- app/Console/Commands/GetMatchesCommand.php
+++ app/Console/Commands/GetMatchesCommand.php
@@ -9,10 +9,14 @@
 namespace App\Console\Commands;
 
 
+use App\HttpClient;
 use Illuminate\Console\Command;
 
 class GetMatchesCommand extends Command
 {
+    private $competitionsId = [2015, 2014, 2021, 2019, 2002, 2017, 2003, 2001];
+    private $apiUrl = 'competitions/{id}/matches';
+
     /**
      * The name and signature of the console command.
      *
@@ -44,10 +48,22 @@ class GetMatchesCommand extends Command
      */
     public function handle()
     {
-        $this->line("Some text");
-        $this->info("Hey, watch this !");
-        $this->comment("Just a comment passing by");
-        $this->question("Why did you do that?");
-        $this->error("Ops, that should not happen.");
+        $executionStartTime = microtime(true);
+        $this->line("[Start] getting All matches");
+
+        foreach ($this->competitionsId as $compId) {
+            $this->line("[Start] getting matches for: ". $compId);
+            $uri = str_replace('{id}', $compId, $this->apiUrl);
+            try {
+                app(HttpClient::class)->getRessources($uri);
+                $this->info("[Success] on getting matches for: ". $compId);
+
+            } catch (\Error $error) {
+                $this->error("[Error] on getting matches for: ". $compId);
+            }
+        }
+        $executionEndTime = microtime(true);
+        $time = $executionEndTime-$executionStartTime;
+        $this->comment("[Time] This script took ". $time ." to execute.");
     }
 }
