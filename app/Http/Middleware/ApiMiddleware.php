fb8e6ab3b04aea429d29020c9373a0f8bd9392d7
heptanol <thamine.alaeddine@gmail.com>
2018-11-11 19:19:46 +0100

    Middleware

--- app/Http/Middleware/ApiMiddleware.php
+++ app/Http/Middleware/ApiMiddleware.php
@@ -15,7 +15,7 @@ class ApiMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if ($request->server()["HTTP_ORIGIN"] == 'http://www.footpaper.info') {
+        if ($request->server() && $request->server()["HTTP_ORIGIN"] == 'http://www.footpaper.info') {
             return $next($request);
         }
         return response('Unauthorized',401);
